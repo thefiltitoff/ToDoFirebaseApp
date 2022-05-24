a7d54d65ef076d4cd786a7bfeb45826340456218
Felix Titov <titow.feliks@yandex.ru>
2022-05-24 20:51:01 +0300

    Initial Commit

---
+++ ToDoFirebaseApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ToDoFirebaseApp
+//
+//  Created by Felix Titov on 5/24/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
